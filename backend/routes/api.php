b0929ae24a628cd2129ad729d4249609a09c2bd0
Amie Love Torcende <torcendeamie@gmail.com>
2026-05-16 13:23:17 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 
-Route::get('/schedule', function () {
+Route::get('/schedule/{day}', function ($day) {
 
     $response = Http::get('https://69f34667bd2396bf530fa847.mockapi.io/schedule');
 
-    return response()->json($response->json());
+    $schedules = collect($response->json());
+
+    $filtered = $schedules->filter(function ($item) use ($day) {
+        return strtolower($item['day']) == strtolower($day);
+    });
+
+    return response()->json($filtered->values());
 
 });
